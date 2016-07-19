e6707b91412d83289ac8d5b1e641edd0849d084a
montagy <jencean@gmail.com>
2016-07-19 16:08:13 +0800

    重写loginModal,去掉了contrib依赖

--- src/Widgets/Login.hs
+++ src/Widgets/Login.hs
@@ -1,14 +1,15 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecursiveDo #-}
 module Widgets.Login (
   loginW
 ) where
 
 import Reflex
 import Reflex.Dom
-import Reflex.Dom.Contrib.Widgets.Modal
 import Common.Types
 import qualified Api
 import Utils
+import Data.Monoid
 
 formControl :: Reflex t => Dynamic t AttributeMap
 formControl = constDyn ("class" =: "form-control")
@@ -20,29 +21,43 @@ loginHeader =
     elAttr "h4" ("class" =: "modal-title") $ text "Modal title"
     pure btn
 
-loginBody :: MonadWidget t m => m (Dynamic t (Either String UserInfo))
-loginBody = do
+loginBody :: MonadWidget t m => m (Event t (Either String UserInfo))
+loginBody = divClass "modal-body" $ do
   name <- textInput $ def & attributes .~ formControl
   pwd <- textInput $ def & attributes .~ formControl
   submit <- buttonAttr "Submit" formControl
   user <- combineDyn User (value name) (value pwd)
-  holdDyn (Left "") =<< Api.login (tagDyn user submit)
+  Api.login (tagDyn user submit)
 
-loginFooter :: MonadWidget t m => Dynamic t (Either String UserInfo) -> m (Event t (), Event t ())
-loginFooter body =
+loginFooter :: MonadWidget t m => m (Event t (), Event t ())
+loginFooter =
   divClass "modal-footer" $ do
     cancel <- buttonAttr "Cancel" formControl
     submit <- buttonAttr "OK" formControl
     pure (cancel, submit)
 
-loginModal :: MonadWidget t m => m (Event t (Either String UserInfo), Event t ())
-loginModal =
-  mkModalBody loginHeader loginFooter loginBody
+loginModal :: MonadWidget t m => Event t () -> m (Event t (Either String UserInfo), Dynamic t AttributeMap)
+loginModal eToggle =
+  divClass "modal-dialog" $ divClass "modal-content" $ do
+    eClose <- loginHeader
+    eeUserInfo <- loginBody
+    (eCancel, eOk) <- loginFooter
+    dModalAttr' <- holdDyn False $
+      leftmost [True<$ eToggle, False<$ eClose, False <$ eCancel, False <$ eOk]
+    dModalAttr <- mapDyn visiblility dModalAttr'
+    pure (eeUserInfo, dModalAttr)
+
+  where
+    visiblility True = "style" =: "display:block;"
+    visiblility False = "style" =: "display:none;"
 
 loginW :: MonadWidget t m => m (Dynamic t (Either String UserInfo))
 loginW = do
-  modal <- button "登陆"
-  dShow <- toggle False modal
-  eeUserInfo <- hidingModal DisplayNone (ModalConfig $ "class" =: "modal") (updated dShow) loginModal
+  rec
+    modal <- button "登陆"
+    dModalAttr' <- mapDyn (("class" =: "modal") <>) dModalAttr
+    (eeUserInfo, dModalAttr) <- elDynAttr "modal" dModalAttr' $
+      loginModal modal
+
   holdDyn (Left "") eeUserInfo
 
